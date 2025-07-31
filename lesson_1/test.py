bdd9dd462a1cba39fcfb2f61e140282418c0b74c
Anton Tymchyk <tony.zenekis@gmail.com>
2025-07-31 22:07:04 +0300

    First commit

---
+++ lesson_1/test.py
@@ -0,0 +1,8 @@
+surname = 'Tymchyk'
+print('Surname:', surname)
+
+length = int(input("Enter length rectangle: "))
+width = int(input("Enter width rectangle: "))
+
+# area = length * width
+print("Area of the rectangle:", length * width) 

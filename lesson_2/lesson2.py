ba80503742ea5b62821ab71bb88c714d9c0839f7
Anton Tymchyk <tony.zenekis@gmail.com>
2025-12-01 23:53:36 +0200

    learning

--- lesson_2/lesson2.py
+++ lesson_2/lesson2.py
@@ -1,12 +1,29 @@
-a = input('Введіть значення: ')
+# client_type = 'basic'
+# order_amount = 1001
 
-print(a[::3])
-print(a[0].upper())
+# discount = (
+#     0.2 if client_type == 'premium' and order_amount > 750 else
+#     0.1 if client_type == 'basic' and order_amount > 1000 else
+#     0
+# )
 
-# -----------------
+# final_price = order_amount * (1 - discount)
 
-surname = input('Введіть прізвище: ')
-first_name = input("Введіть ім'я: ")
-patronymic = input('Введіть по батькові: ')
+# print(f'Тип клієнта: {client_type} \n Сумма замовлення: {order_amount} грн. \n Знижка: {round(discount * 100)}% \n Фінальна ціна: {final_price} грн.')
 
-print(f"{surname.title()} {first_name.title()} {patronymic.title()}")
+password = 'hello'
+counter = 3
+
+while counter >= 1:
+    inputPassword = input('Введіть пароль: ')
+    if inputPassword == password:
+        print('Удача, пароль правильний')
+        break
+    else:
+        counter -= 1
+        if counter >= 1:
+            print(f'Помилка, у вас лишилось {counter} спроб')
+        
+if counter < 1:
+    print('Ви перевищили ліміт спроб!')
+    

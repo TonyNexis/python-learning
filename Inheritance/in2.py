f9253e3360fc9193bbfd124033887cedbf956d8e
Anton Tymchyk <tony.zenekis@gmail.com>
2026-01-21 22:30:15 +0200

    Inheritance learning

---
+++ Inheritance/in2.py
@@ -0,0 +1,13 @@
+from inheritance import NewStore
+
+class BeerStore(NewStore):
+    water = 10000
+    beer = 20
+
+    # @classmethod
+    def info(self):
+        storeInfo = super().info()
+        return storeInfo + f' Alcohol: {self.beer} beer.'
+    
+gradus = BeerStore('Gradus')
+print(gradus.info())

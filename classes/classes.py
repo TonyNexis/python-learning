7b10b729a04accb7d7249842396ff29a77b476f3
Anton Tymchyk <tony.zenekis@gmail.com>
2026-03-18 21:43:56 +0200

    added classes

--- classes/classes.py
+++ classes/classes.py
@@ -1,141 +1,162 @@
-# from dataclasses import dataclass
-# from typing import ClassVar
+# # from dataclasses import dataclass
+# # from typing import ClassVar
 
 
-# class Test: 
-#     def __init__(
-#             self,
-#             side_text,
-#             main_color,
-#             additional_colors,
-#             autopilot
-#     ):
-#         self.side_text = side_text
-#         self.main_color = main_color
-#         self.additional_colors = additional_colors
-#         self.autopilot = autopilot
+# # class Test: 
+# #     def __init__(
+# #             self,
+# #             side_text,
+# #             main_color,
+# #             additional_colors,
+# #             autopilot
+# #     ):
+# #         self.side_text = side_text
+# #         self.main_color = main_color
+# #         self.additional_colors = additional_colors
+# #         self.autopilot = autopilot
 
-#     def view_info(self):
-#         print(f'some info ==> {self.side_text}')
+# #     def view_info(self):
+# #         print(f'some info ==> {self.side_text}')
 
-# @dataclass(frozen=True)
-# class Plane:
-#     wings_count: ClassVar[int] = 2
+# # @dataclass(frozen=True)
+# # class Plane:
+# #     wings_count: ClassVar[int] = 2
 
-#     side_text: str
-#     main_color: str
-#     additional_colors: str
-#     autopilot: bool
+# #     side_text: str
+# #     main_color: str
+# #     additional_colors: str
+# #     autopilot: bool
 
-#     @classmethod
-#     def changewings_num(cls, new_wings_count: int):
-#         cls.wings_count = new_wings_count
+# #     @classmethod
+# #     def changewings_num(cls, new_wings_count: int):
+# #         cls.wings_count = new_wings_count
 
-# raptor = Test(
-#     side_text='Raptor 323',
-#     main_color='red',
-#     additional_colors=['black', 'white'],
-#     autopilot='false'
-# )
+# # raptor = Test(
+# #     side_text='Raptor 323',
+# #     main_color='red',
+# #     additional_colors=['black', 'white'],
+# #     autopilot='false'
+# # )
 
-# boing = Plane('boing 123', 'white', 'blue', True)
-# kukuruza = Plane('Kukuruza 1', 'black', 'white', False)
+# # boing = Plane('boing 123', 'white', 'blue', True)
+# # kukuruza = Plane('Kukuruza 1', 'black', 'white', False)
 
-# # print(f'Raptor colors ->> {', '.join(raptor.additional_colors)}')
-# # print(f'Boing ->> {boing}')
-# boing.changewings_num(3)
-# print(boing.wings_count)
-# print(kukuruza.wings_count)
-
-
-user_name = 'Anton'
-user_age = 35
-languages = ['Python', 'Javascript', 'Typescript']
-
-# print(f'Привіт, мене звати {user_name}, мені {user_age} років і я вчу {languages[0]}')
-
-experience_years = 0
-
-if experience_years >= 2:
-    print('Ти вже Middle розробник!')
-elif experience_years > 0:
-    print('Ти впевнений Junior!')
-else:
-    print('Ти на початку шляху, успіхів у навчанні!')
-
-if "Python" in languages:
-    print('О, ти обрав правильну мову!')
-
-tech_stack = ["Python", "FastAPI", "SQL", "Docker", "Linux"]
-
-for i in tech_stack:
-    if i == 'Python':
-        print(f'{i} - це основа!')
-    elif i == 'Docker':
-        print(f'{i} - це складно, але корисно!')
-    else:
-        print(f'Вивчаю {i}...')
-
-secret_password = "python123"
-user_input = ''
-attempts = 0
-
-# while user_input != secret_password:
-#     attempts += 1
-#     print(f'Спроба №{attempts}...')
-#     user_input = input('Введіть пароль: ')
-
-# print(f'Доступ дозволено! Кількість ваших спроб: {attempts}')
-
-def get_long_names(names: list[str]) -> list[str]:
-    """
-    Фільтрує список імен, залишаючи лише ті, що мають більше 5 символів.
-
-    Args:
-        names (list[str]): Список імен (рядків), які потрібно перевірити.
-
-    Returns:
-        list[str]: Новий список, що містить лише імена довжиною понад 5 символів.
-    """
-
-    result = []
-
-    for name in names:
-        if len(name) > 5:
-            result.append(name)
-
-    return result
-
-some_names = ["Vova", "Anton", "Jakonda", "Valentina", "Volodimir"]
-
-print(get_long_names(some_names))
-
-def new_test_func(names: list[str]) -> list[str]:
-    return [name for name in names if len(name) > 5]
-
-print(new_test_func(some_names))
-
-def convert_prices(prices: list[float]) -> list[float]:
-    """Конвертує ціни > 1000 грн у долари."""
-    return [price/40 for price in prices if price > 1000]
-
-price_list = [400.0, 2000.0, 80.0, 4000.0]
-
-print(convert_prices(price_list))
-
-items = [
-    {"name": "Phone", "price": 15000},
-    {"name": "Case", "price": 500},
-    {"name": "Laptop", "price": 45000},
-    {"name": "Cable", "price": 300}
-]
-
-dirty_items = [
-    {"name": "Phone", "price": 15000},
-    {"price": 50000},  # Немає імені
-    {"name": "Mystery Box"},  # Немає ціни
-    {"name": "Case", "price": 200}
-]
+# # # print(f'Raptor colors ->> {', '.join(raptor.additional_colors)}')
+# # # print(f'Boing ->> {boing}')
+# # boing.changewings_num(3)
+# # print(boing.wings_count)
+# # print(kukuruza.wings_count)
 
 
+# user_name = 'Anton'
+# user_age = 35
+# languages = ['Python', 'Javascript', 'Typescript']
 
+# # print(f'Привіт, мене звати {user_name}, мені {user_age} років і я вчу {languages[0]}')
+
+# experience_years = 0
+
+# if experience_years >= 2:
+#     print('Ти вже Middle розробник!')
+# elif experience_years > 0:
+#     print('Ти впевнений Junior!')
+# else:
+#     print('Ти на початку шляху, успіхів у навчанні!')
+
+# if "Python" in languages:
+#     print('О, ти обрав правильну мову!')
+
+# tech_stack = ["Python", "FastAPI", "SQL", "Docker", "Linux"]
+
+# for i in tech_stack:
+#     if i == 'Python':
+#         print(f'{i} - це основа!')
+#     elif i == 'Docker':
+#         print(f'{i} - це складно, але корисно!')
+#     else:
+#         print(f'Вивчаю {i}...')
+
+# secret_password = "python123"
+# user_input = ''
+# attempts = 0
+
+# # while user_input != secret_password:
+# #     attempts += 1
+# #     print(f'Спроба №{attempts}...')
+# #     user_input = input('Введіть пароль: ')
+
+# # print(f'Доступ дозволено! Кількість ваших спроб: {attempts}')
+
+# def get_long_names(names: list[str]) -> list[str]:
+#     """
+#     Фільтрує список імен, залишаючи лише ті, що мають більше 5 символів.
+
+#     Args:
+#         names (list[str]): Список імен (рядків), які потрібно перевірити.
+
+#     Returns:
+#         list[str]: Новий список, що містить лише імена довжиною понад 5 символів.
+#     """
+
+#     result = []
+
+#     for name in names:
+#         if len(name) > 5:
+#             result.append(name)
+
+#     return result
+
+# some_names = ["Vova", "Anton", "Jakonda", "Valentina", "Volodimir"]
+
+# print(get_long_names(some_names))
+
+# def new_test_func(names: list[str]) -> list[str]:
+#     return [name for name in names if len(name) > 5]
+
+# print(new_test_func(some_names))
+
+# def convert_prices(prices: list[float]) -> list[float]:
+#     """Конвертує ціни > 1000 грн у долари."""
+#     return [price/40 for price in prices if price > 1000]
+
+# price_list = [400.0, 2000.0, 80.0, 4000.0]
+
+# print(convert_prices(price_list))
+
+# items = [
+#     {"name": "Phone", "price": 15000},
+#     {"name": "Case", "price": 500},
+#     {"name": "Laptop", "price": 45000},
+#     {"name": "Cable", "price": 300}
+# ]
+
+# dirty_items = [
+#     {"name": "Phone", "price": 15000},
+#     {"price": 50000},  # Немає імені
+#     {"name": "Mystery Box"},  # Немає ціни
+#     {"name": "Case", "price": 200}
+# ]
+
+
+class BankAccount:
+    def __init__(self, owner: str, balance: float = 0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount: float):
+        self.balance += amount
+        print(f'Рахунок поповнено на {amount}. Новий баланс: {self.balance}')
+    
+    def withdraw(self, amount: int):
+        if amount > self.balance:
+            print('Недостатньо коштів на рахунку!')
+            return
+        self.balance -= amount
+        print(f'З рахунка знято {amount}, Новий баланс: {self.balance}')
+
+
+    
+
+my_acc = BankAccount("Taras", 1000)
+my_acc.deposit(500)
+my_acc.withdraw(200) # Має написати про нестачу коштів
--- lesson_1/main.py
+++ lesson_1/main.py
@@ -3,11 +3,14 @@ from validators import save_price_check
 def get_expensive_items(items: list[dict]) -> list[str]:
     return [item.get('name', 'Unknown Item') for item in items if save_price_check(item)]
 
-broken_items = [
+data = [
     {"name": "Phone", "price": 10000},
     {"name": "Laptop", "price": "ціна за запитом"},
     {"price": 5000}, # Немає імені
     {"name": "Old Cable", "price": None} # Ціна є, але вона порожня
 ]
 
-print(get_expensive_items(broken_items))
+if __name__ == '__main__':
+    result = get_expensive_items(data)
+    print(f'Знайдено дорогіх товарів: {result}')
+
